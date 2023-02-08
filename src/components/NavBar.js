2c6cc3caa607bf7af630a8d145e62f0629792e15
Jody M <jodyamm@hotmail.com>
2023-02-08 11:26:17 +0000

    Started navbar, changed colour of background and import logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Container, Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap'
+import styles from "../styles/NavBar.module.css";
+import logo from '../assets/logo.png'
 
 const NavBar = () => {
-  return (
-    <div>NavBar placeholder text</div>
-  )
+    return (
+        <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
+            <Navbar.Brand href="#home"><img src={logo} alt="logo" height="50" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    )
 }
 
 export default NavBar
