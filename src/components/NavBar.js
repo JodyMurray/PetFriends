ccc646042eda7e9e7e5229e2db08871bf423fbf2
Jody M <jodyamm@hotmail.com>
2023-02-09 19:08:53 +0000

    Add hover effect to navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,21 +5,22 @@ import logo from '../assets/logo.png'
 
 const NavBar = () => {
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
-            <Navbar.Brand>
-                <img src={logo} alt="logo" height="50" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" className='navbarToggle'/>
-            <Navbar.Collapse id="navbarScroll">
-                <Nav className="mx-auto">
-                    <Nav.Link>Home <i class="fas fa-house-chimney-window" ></i></Nav.Link>
-                    <Nav.Link>Sign in <i class="fas fa-circle-right"></i></Nav.Link>
-                    <Nav.Link>Sign up <i class="fas fa-square-plus"></i></Nav.Link>
-                </Nav>
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="50" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" className='navbarToggle' />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="mx-auto">
+                        <Nav.Link className={styles.NavLink}>Home <i class="fas fa-house-chimney-window" ></i></Nav.Link>
+                        <Nav.Link className={styles.NavLink}>Sign in <i class="fas fa-circle-right"></i></Nav.Link>
+                        <Nav.Link className={styles.NavLink}>Sign up <i class="fas fa-square-plus"></i></Nav.Link>
+                        <Nav.Link className={styles.NavLink}>About <i class="fa-regular fa-circle-question"></i></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
 
-            </Navbar.Collapse>
-        
-        </Container>
+            </Container>
         </Navbar>
     )
 }
