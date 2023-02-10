1de6623bba8d50fa9248d71ebba0b11cbd2d049d
Jody M <jodyamm@hotmail.com>
2023-02-10 16:40:32 +0000

    Navlink logo style added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,12 +7,12 @@ const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand>
+                <Navbar.Brand className={styles.NavLink}>
                     <img src={logo} alt="logo" height="50" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" className='navbarToggle' />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="mx-auto">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" className='navbarToggle' />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="my-auto">
                         <Nav.Link className={styles.NavLink}>Home <i class="fas fa-house-chimney-window" ></i></Nav.Link>
                         <Nav.Link className={styles.NavLink}>Sign in <i class="fas fa-circle-right"></i></Nav.Link>
                         <Nav.Link className={styles.NavLink}>Sign up <i class="fas fa-square-plus"></i></Nav.Link>
