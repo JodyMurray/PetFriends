24ffaadae20d547e42b2afe76e64381b633ef208
Jody M <jodyamm@hotmail.com>
2023-02-18 07:37:26 +0000

    Create data for postData and setPostData

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
@@ -7,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
 import Upload from "../../assets/upload.png";
+import { useHistory } from "react-router";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
@@ -14,7 +14,33 @@ import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 
 function PostCreateForm() {
+    const [postData, setPostData] = useState({
+        title: "",
+        content: "",
+        image: "",
+        imageFilter: "",
+    });
+    const { title, content, image, imageFilter } = postData;
 
+    const imageInput = useRef(null);
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setPostData({
+            ...postData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleChangeImage = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(image);
+            setPostData({
+                ...postData,
+                image: URL.createObjectURL(event.target.files[0]),
+            });
+        }
+    };
     const [errors, setErrors] = useState({});
 
 
@@ -22,12 +48,15 @@ function PostCreateForm() {
         <div className="text-center">
             <Form.Group>
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" name="title" />
+                <Form.Control type="text" name="title" value={title}
+                    onChange={handleChange} />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Content</Form.Label>
-                <Form.Control as="textarea" rows={7} name="content" />
+                <Form.Control as="textarea" rows={6} name="content" value={content}
+                    onChange={handleChange} />
             </Form.Group>
+
             <Button className={`${btnStyles.Button} ${btnStyles.Light}`} type="submit">
                 create
             </Button>
@@ -57,6 +86,10 @@ function PostCreateForm() {
                             >
                                 <Asset src={Upload} message="Upload an image here!" />
                             </Form.Label>
+                            <Form.File
+                                id="image-upload"
+                                accept="image/*"
+                            />
                         </Form.Group>
                         <div className="d-md-none">{textFields}</div>
                     </Container>
