f16502eb42a6576a110462f11d9bafd4bb67cdb6
Jody M <jodyamm@hotmail.com>
2023-02-18 07:43:36 +0000

    Add Image component and import from react

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 import Upload from "../../assets/upload.png";
 import { useHistory } from "react-router";
@@ -80,15 +81,35 @@ function PostCreateForm() {
                         className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
                     >
                         <Form.Group className="text-center">
-                            <Form.Label
-                                className="d-flex justify-content-center"
-                                htmlFor="image-upload"
-                            >
-                                <Asset src={Upload} message="Upload an image here!" />
-                            </Form.Label>
+                            {image ? (
+                                <>
+                                    <figure>
+                                        <Image className={appStyles.Image} src={image} rounded />
+                                    </figure>
+                                    <div>
+                                        <Form.Label
+                                            className={`${btnStyles.Button} ${btnStyles.Light} btn`}
+                                            htmlFor="image-upload"
+                                        >
+                                            Change the image
+                                        </Form.Label>
+                                    </div>
+                                </>
+                            ) : (
+                                <Form.Label
+                                    className="d-flex justify-content-center"
+                                    htmlFor="image-upload"
+                                >
+                                    <Asset
+                                        src={Upload}
+                                        message="Click or tap to upload an image"
+                                    />
+                                </Form.Label>
+                            )}
                             <Form.File
                                 id="image-upload"
                                 accept="image/*"
+                                onChange={handleChangeImage}
                             />
                         </Form.Group>
                         <div className="d-md-none">{textFields}</div>
