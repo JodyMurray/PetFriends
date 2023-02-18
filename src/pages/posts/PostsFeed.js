e08ab422321d0723b50b1a6a4da66c4ea86cfa28
Jody M <jodyamm@hotmail.com>
2023-02-18 15:19:36 +0000

    Import useParams and useState in PostsFeed.js

--- src/pages/posts/PostsFeed.js
+++ src/pages/posts/PostsFeed.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function PostsFeed() {
-
+    const {id} = useParams();
+    const [post, setPost] = useState();
 
     return (
         <Row className="h-100">
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-                Popular profiles for desktop
+                placeholder
             </Col>
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Profiles</p>
