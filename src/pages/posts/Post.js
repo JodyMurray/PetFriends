8e9f3281ba2b2c1775758433f5e02f63f94b3eb2
Jody M <jodyamm@hotmail.com>
2023-02-18 15:49:31 +0000

    Add props to Post.js

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,9 +2,28 @@ import React from 'react'
 import styles from '../../styles/Post.module.css';
 
 const Post = (props) => {
-  return (
-    <div>Post</div>
-  )
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        reply_count,
+        votes_count,
+        downvotes_count,
+        vote_id,
+        saved_id,
+        downvotes_id,
+        title,
+        content,
+        image,
+        updated_at,
+        postFeed,
+    } = props;
+
+    
+    return (
+        <div>Post</div>
+    )
 }
 
 export default Post
