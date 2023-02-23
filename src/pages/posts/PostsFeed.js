34584453117bbb69ecb03c837b16546f24c1ad4e
Jody M <jodyamm@hotmail.com>
2023-02-23 14:44:21 +0000

    Add Asset spinner, loader and hasMore into InfiniteScroll

--- src/pages/posts/PostsFeed.js
+++ src/pages/posts/PostsFeed.js
@@ -69,6 +69,10 @@ function PostsFeed({ message, filter = "" }) {
                                         <Post key={post.id} {...post} setPosts={setPosts} />
                                     ))
                                 }
+                                dataLength={posts.results.length}
+                                loader={<Asset spinner />}
+                                hasMore={!!posts.next}
+                                next={()=> {}}
                             />
                         ) : (
                             <Container className={appStyles.Content}>
