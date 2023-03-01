396fe3afa1eeb0591606a29e53cbf985aced1685
Jody M <jodyamm@hotmail.com>
2023-03-01 11:48:18 +0000

    Create ProfileDataContext.js file inside contexts folder and set up objects

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,5 @@
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
