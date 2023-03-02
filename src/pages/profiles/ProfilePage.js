fc50daf41377c93253d45c6a0216dcf6b26726b4
Jody M <jodyamm@hotmail.com>
2023-03-02 18:28:40 +0000

    Create ProfilePage.js file, import styles and start function code

---
+++ src/pages/profiles/ProfilePage.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styles from "../../styles/ProfilePage.module.css";
+import appStyles from "../../App.module.css";
+import PopularProfiles from "./PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+
+function ProfilePage() {
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const currentUser = useCurrentUser();
+}
+
+
+export default ProfilePage;
