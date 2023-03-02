2b29c078a141052c8199f1360a7f0200276a9522
Jody M <jodyamm@hotmail.com>
2023-03-02 18:38:14 +0000

    Add route for ProfilePage in app.js file

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostFeed from './pages/posts/PostFeed';
 import PostsFeed from './pages/posts/PostsFeed';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import EditPostForm from './pages/posts/EditPostForm';
+import ProfilePage from './pages/profiles/ProfilePage';
 
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
           <Route exact path='/about' render={() => <h1>About</h1>} />
           <Route exact path='/posts/create' render={() => <PostCreateForm />} />
           <Route exact path='/posts/:id/edit' render={() => <EditPostForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route exact path='/posts/:id' render={() => <PostFeed />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/ProfilePage.module.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -6,41 +6,39 @@ import appStyles from "../../App.module.css";
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
-
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
     const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        setHasLoaded(true);
+    }, [])
+
+
+    const mainProfile = (
+        <>
+            <Row noGutters className="px-3 text-center">
+                <Col lg={3} className="text-lg-left">
+                    <p>Image</p>
+                </Col>
+                <Col lg={6}>
+                    <h3 className="m-2">Username</h3>
+                    <p>Stats</p>
+                </Col>
+                <Col lg={3} className="text-lg-right">
+                    <p>Follow button</p>
+                </Col>
+                <Col className="p-3">Content</Col>
+            </Row>
+        </>
+    );
+
+    const mainProfilePosts = (
+        <>
+            <hr />
+            <p className="text-center">Your posts</p>
+            <hr />
+        </>
+    );
 }
-
-useEffect(() => {
-    setHasLoaded(true);
-}, [])
-
-
-const mainProfile = (
-    <>
-        <Row noGutters className="px-3 text-center">
-            <Col lg={3} className="text-lg-left">
-                <p>Image</p>
-            </Col>
-            <Col lg={6}>
-                <h3 className="m-2">Username</h3>
-                <p>Stats</p>
-            </Col>
-            <Col lg={3} className="text-lg-right">
-                <p>Follow button</p>
-            </Col>
-            <Col className="p-3">Content</Col>
-        </Row>
-    </>
-);
-
-const mainProfilePosts = (
-    <>
-        <hr />
-        <p className="text-center">Your posts</p>
-        <hr />
-    </>
-);
-
 export default ProfilePage;
