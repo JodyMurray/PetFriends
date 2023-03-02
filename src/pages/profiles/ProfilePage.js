8c38772cf8a34a12840b92677b4e8756a1426987
Jody M <jodyamm@hotmail.com>
2023-03-02 18:33:16 +0000

    Create mainProfile and mainProfilePosts and add col and row with placeholder text

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styles from "../../styles/ProfilePage.module.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import appStyles from "../../App.module.css";
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -14,5 +16,31 @@ useEffect(() => {
     setHasLoaded(true);
 }, [])
 
+
+const mainProfile = (
+    <>
+        <Row noGutters className="px-3 text-center">
+            <Col lg={3} className="text-lg-left">
+                <p>Image</p>
+            </Col>
+            <Col lg={6}>
+                <h3 className="m-2">Username</h3>
+                <p>Stats</p>
+            </Col>
+            <Col lg={3} className="text-lg-right">
+                <p>Follow button</p>
+            </Col>
+            <Col className="p-3">Content</Col>
+        </Row>
+    </>
+);
+
+const mainProfilePosts = (
+    <>
+        <hr />
+        <p className="text-center">Your posts</p>
+        <hr />
+    </>
+);
 
 export default ProfilePage;
