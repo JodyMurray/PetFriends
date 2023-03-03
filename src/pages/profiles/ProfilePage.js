5993f806d33ac9da38aa2d29d6e57aa8e8d3ad69
Jody M <jodyamm@hotmail.com>
2023-03-03 18:00:44 +0000

    Add Follow and Unfollow button logic to row in profilePage

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Asset from "../../components/Asset";
 import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useParams } from "react-router";
@@ -13,8 +14,8 @@ import {
     useProfileData,
     useSetProfileData,
 } from "../../contexts/ProfileDataContext";
-import { Image } from "react-bootstrap";
-import Post from "../posts/Post";
+import { Button, Image } from "react-bootstrap";
+
 
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -23,6 +24,7 @@ function ProfilePage() {
     const setProfileData = useSetProfileData();
     const { pageProfile } = useProfileData();
     const [profile] = pageProfile.results;
+    const is_owner = currentUser?.username === profile?.owner;
 
     useEffect(() => {
         const fetchData = async () => {
@@ -51,6 +53,8 @@ function ProfilePage() {
                         roundedCircle
                         src={profile?.image}
                     />
+
+
                 </Col>
                 <Col lg={6}>
                     <h3 className="m-2">{profile?.owner}</h3>
@@ -67,12 +71,28 @@ function ProfilePage() {
                             <div>{profile?.following_count}</div>
                             <div>following</div>
                         </Col>
+                        <Col lg={3} className="text-lg-right">
+                            {currentUser &&
+                                !is_owner &&
+                                (profile?.following_id ? (
+                                    <Button
+                                        className={`${btnStyles.Button} ${btnStyles.Light}`}
+                                        onClick={() => { }}
+                                    >
+                                        unfollow
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        className={`${btnStyles.Button} ${btnStyles.Follow}`}
+                                        onClick={() => { }}
+                                    >
+                                        follow
+                                    </Button>
+                                ))}
+                        </Col>
                     </Row>
                 </Col>
-                <Col lg={3} className="text-lg-right">
-                    <p>Follow button</p>
-                </Col>
-                <Col className="p-3">Content</Col>
+                <Col className="col-12 p-5">Content</Col>
             </Row>
         </>
     );
