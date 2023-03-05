aeb357ad9b758e925ad0b1a9230ddfd4804902c5
Jody M <jodyamm@hotmail.com>
2023-03-05 07:01:19 +0000

    Create handleUnfollow and add to onClick attributes

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -27,7 +27,7 @@ function ProfilePage() {
 
     const currentUser = useCurrentUser();
     const { id } = useParams();
-    const { setProfileData, handleFollow } = useSetProfileData();
+    const { setProfileData, handleFollow, handleUnfollow } = useSetProfileData();
     const { pageProfile } = useProfileData();
     const [profile] = pageProfile.results;
     const is_owner = currentUser?.username === profile?.owner;
@@ -88,14 +88,14 @@ function ProfilePage() {
                                 (profile?.following_id ? (
                                     <Button
                                         className={btnStyles.UnFollow}
-                                        onClick={() => { }}
+                                        onClick={() => { handleUnfollow(profile) }}
                                     >
                                         unfollow
                                     </Button>
                                 ) : (
                                     <Button
                                         className={`${btnStyles.Button} ${btnStyles.Follow}`}
-                                        onClick={() => { handleFollow (profile)}}
+                                        onClick={() => { handleFollow(profile) }}
                                     >
                                         follow
                                     </Button>
