6b17ea1fdcb306e6673499b36bd631360cd852f1
Jody M <jodyamm@hotmail.com>
2023-03-06 16:02:11 +0000

    Import UseHistory into MoreDropdown file and create ProfileEditDropdown function

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router";
 
 
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
@@ -13,6 +14,38 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
         }}
     />
 ));
+
+
+export function ProfileEditDropdown({ id }) {
+    const history = useHistory();
+    return (
+      <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="right">
+        <Dropdown.Toggle as={ThreeDots} />
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit`)}
+            aria-label="edit-profile"
+          >
+            <i className="fas fa-user-pen" /> Edit profile
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/username`)}
+            aria-label="edit-username"
+          >
+            <i className="fas fa-user" />
+            Change username
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/password`)}
+            aria-label="edit-password"
+          >
+            <i className="fas fa-lock" />
+            Change password
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
 
 export const MoreDropdown = ({ handleEdit, handleDelete }) => {
     return (
