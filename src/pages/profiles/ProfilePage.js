8dd79aa21cfeba36beb97335b73569b39590cc55
Jody M <jodyamm@hotmail.com>
2023-03-06 16:05:05 +0000

    Import ProfileEditDropdown into ProfilePage.js and add into mainProfile

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -19,6 +19,8 @@ import {
 } from "../../contexts/ProfileDataContext";
 import { Button, Image } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
+
 
 
 function ProfilePage() {
@@ -55,6 +57,7 @@ function ProfilePage() {
 
     const mainProfile = (
         <>
+            {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
             <Row noGutters className="px-3 text-center">
                 <Col lg={3} className="text-lg-left">
 
