64a53c5843e536b2d353767496e3c1d961c12c28
Jody M <jodyamm@hotmail.com>
2023-03-09 13:13:31 +0000

    Set up project for unit tests

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,3 +1,21 @@
-const baseURL = ""
+import { rest } from 'msw';
+const baseURL = 'https://petfriends-api.herokuapp.com/'
 
-export const handlers = []
+export const handlers = [
+    rest.get(`${baseUrl}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json(
+            {
+                "pk": 3,
+                "username": "Finn",
+                "email": "",
+                "first_name": "",
+                "last_name": "",
+                "profile_id": 3,
+                "profile_image": "https://res.cloudinary.com/dmqu7iqfu/image/upload/v1/media/images/Screenshot_2023-02-23_at_16.06.15_p8loaq"
+            })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req,res,ctx) => {
+        return res(ctx.status(200));
+    })
+];
