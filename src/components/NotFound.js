381dd1fdda11c2505d15271a364d7dd878dbebd5
Jody M <jodyamm@hotmail.com>
2023-03-13 08:42:31 +0000

    Create NotFound.js file inside components folder and import NoResults.png

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NoResults from "../../assets/noresults.png";
+
+
+const NotFound = () => {
+  return (
+    <div>NotFound</div>
+  )
+}
+
+export default NotFound
